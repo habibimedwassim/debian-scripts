72b2dfac168c50238679848654a67401b005f553
Habibi Med Wassim <habibimedwassim@gmail.com>
2026-02-17 23:56:03 +0100

    Update graphics-mesa.sh to include additional Vulkan packages and dependencies

--- desktop/graphics-mesa.sh
+++ desktop/graphics-mesa.sh
@@ -7,8 +7,13 @@ sudo apt install -y \
   mesa-vulkan-drivers \
   mesa-vulkan-drivers:i386 \
   intel-media-va-driver-non-free \
-  vainfo \
-  libvulkan-dev
+  libva2 \
+  libva2:i386 \
+  libvulkan1 \
+  libvulkan1:i386 \
+  vulkan-tools \
+  vulkan-validationlayers \
+  vainfo 
 
 sudo apt remove -y xserver-xorg-video-intel
 sudo apt autoremove -y
