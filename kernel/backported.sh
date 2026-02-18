25514a9f5219af85e0df74091458ad08c2fc9232
Habibi Med Wassim <habibimedwassim@gmail.com>
2026-02-18 01:12:07 +0100

    Update backported.sh; add checks for Debian Trixie and backports availability

--- kernel/backported.sh
+++ kernel/backported.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
+# check if running on trixie not forky or sid or others, if not, exit with message
+if ! grep -q "VERSION_CODENAME=trixie" /etc/os-release; then
+    echo "This script is intended for Debian Trixie. Exiting."
+    exit 1
+fi
+
+#check if backports is already enabled
+if ! grep -q "deb.*trixie-backports" /etc/apt/sources.list /etc/apt/sources.list.d/*.list 2>/dev/null; then
+    echo "deb http://deb.debian.org/debian trixie-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
+    sudo apt update
+else
+    echo "Backports already enabled. Skipping."
+fi
 
 sudo apt install -t trixie-backports linux-headers-amd64 linux-image-amd64 -y
